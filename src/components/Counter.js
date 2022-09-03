9725c8d35d4b4f30170452a5e359c78758b2f78e
Biswajit Mukherjee <imbiswajit30@gmail.com>
2022-09-03 20:17:58 +0530

    Initial commit.
    Created a counter app with cra template.

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import styles from "./Counter.module.css";
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementHandler = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const decrementHandler = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
+  return (
+    <div className={styles["counter"]}>
+      <p className={styles["counter__value"]}>{counter}</p>
+      <div className={styles["counter__actions"]}>
+        <button className={styles["btn"]} onClick={decrementHandler}>
+          Decrement
+        </button>
+        <button className={styles["btn"]} onClick={incrementHandler}>
+          Increment
+        </button>
+      </div>
+    </div>
+  );
+}

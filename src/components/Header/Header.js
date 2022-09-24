f0c16911d3f0b20392e4699677ec4640280205b1
Biswajit Mukherjee <76398740+Biswajit-Mukherjee@users.noreply.github.com>
2022-09-24 14:25:08 +0530

    Update Header Logo url
    Fetch Header logo from Cloudinary

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import styles from "./Header.module.css";
 
-const HEADER_LOGO = "https://i.ibb.co/VDr5dkY/logo.jpg";
+// const HEADER_LOGO = "https://i.ibb.co/VDr5dkY/logo.jpg";
+const HEADER_LOGO = "https://res.cloudinary.com/dbnt8bskc/image/upload/v1664009616/Projects/countX/logo_zytw6b.jpg";
 const HEADER_TITLE = "CountX";
 
 export default function Header() {

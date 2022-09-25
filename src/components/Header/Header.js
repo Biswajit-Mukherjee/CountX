b1741622207d81aa716801e6efbb15e0ed129de6
Biswajit Mukherjee <imbiswajit30@gmail.com>
2022-09-25 18:22:11 +0530

    Update Header Logo url

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import styles from "./Header.module.css";
 
-const HEADER_LOGO = "https://i.ibb.co/VDr5dkY/logo.jpg";
+const HEADER_LOGO =
+  "https://res.cloudinary.com/biswajit-mukherjee/image/upload/v1664009616/Projects/countX/logo_zytw6b.jpg";
 const HEADER_TITLE = "CountX";
 
 export default function Header() {
@@ -9,7 +10,9 @@ export default function Header() {
       <div className={styles["header__logo"]}>
         <img src={HEADER_LOGO} alt="App logo" />
       </div>
-      <h1 className={styles["header__title"]}><code>{HEADER_TITLE}</code></h1>
+      <h1 className={styles["header__title"]}>
+        <code>{HEADER_TITLE}</code>
+      </h1>
     </header>
   );
 }
